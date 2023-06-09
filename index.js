37a9161d4c2b29e7c18ec79fe1b20a81d32b3a69
unknown <you@example.com>
2023-06-08 22:51:41 -0700

    added classes get api

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app=express()
 require('dotenv').config()
 const port=process.env.PORT||5000;
 const instructor=require('./instructors.json')
+const classes=require('./classes.json')
 
 app.use(cors())
 app.use(express.json())
@@ -16,6 +17,10 @@ app.get('/',(req,res)=>{
 
 app.get('/instructor',(req,res)=>{
     res.send(instructor)
+})
+
+app.get('/classes',(req,res)=>{
+    res.send(classes)
 })
 
 app.listen(port,()=>{
