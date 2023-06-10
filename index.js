acde8723053a24669c9532419a28ce6ca23d7eac
unknown <you@example.com>
2023-06-10 10:54:19 +0600

    added user get and post api

--- index.js
+++ index.js
@@ -30,11 +30,35 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const userCollection=client.db('vacationDb').collection('user')
     const addCollection=client.db('vacationDb').collection('addclass');
 
+
+
+    app.get('/users',async(req,res)=>{
+      const result=await userCollection.find().toArray()
+      res.send(result)
+    })
+
+    app.post('/users',async(req,res)=>{
+      const user=req.body;
+      const query={email:user.email};
+      const existingUser=await userCollection.findOne(query);
+      if(existingUser){
+        return res.send({message:'User Already Have Exists'})
+      }
+      const result=await userCollection.insertOne(user)
+      res.send(result)
+    })
+
+
+
+
+
+
     app.post('/addclass',async(req,res)=>{
-        const item=req.body;
-     const result=await addCollection.insertOne(item)
+      const item=req.body;
+      const result=await addCollection.insertOne(item)
      res.send(result)
     });
 
