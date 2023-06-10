64077c5ff5d87727745320e3c59644182e86353a
unknown <you@example.com>
2023-06-10 21:02:14 +0600

    added patch method

--- index.js
+++ index.js
@@ -40,6 +40,16 @@ async function run() {
       res.send(result)
     })
 
+    app.get('/users/admin/:email',async(req,res)=>{
+      const email=req.params.email;
+      const query={email:email};
+      const user=await userCollection.findOne(query)
+      const result={admin:user?.role==='admin'}
+      res.send(result)
+    })
+
+
+
     app.post('/users',async(req,res)=>{
       const user=req.body;
       const query={email:user.email};
@@ -57,6 +67,18 @@ async function run() {
       const updateDoc={
         $set:{
           role:'admin'
+        }
+      }
+      const result=await userCollection.updateOne(filter,updateDoc);
+      res.send(result)
+    })
+
+    app.patch('/users/instructor/:id',async(req,res)=>{
+      const id=req.params.id;
+      const filter={_id:new ObjectId(id)};
+      const updateDoc={
+        $set:{
+          role:'instructor'
         }
       }
       const result=await userCollection.updateOne(filter,updateDoc);
